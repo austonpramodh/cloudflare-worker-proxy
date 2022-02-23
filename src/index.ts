84f06a794d186adcf38d7fdd0e8c3f7479076aeb
Auston Pramodh Barboza <auston.barboza@cometchat.com>
2022-02-22 21:19:50 -0500

    chore: updated dev deps

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { handleRequest } from "./handler";
 
 addEventListener("fetch", (event) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore need to check this issue
     event.respondWith(handleRequest(event.request));
 });
